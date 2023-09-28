42c14f08fad87f976a222b0b69f791b7c87085d4
AhmadD4 <s362074@oslomet.no>
2023-09-28 16:13:08 +0200

    Remove the upload images and retry it…

--- AirMet/Controllers/HomeController.cs
+++ AirMet/Controllers/HomeController.cs
@@ -7,11 +7,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using AirMet.ViewModels;
 using Microsoft.EntityFrameworkCore;
-using static System.Net.Mime.MediaTypeNames;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Processing;
-using SixLabors.ImageSharp.Formats;
-using SixLabors.ImageSharp.Formats.Jpeg;
 
 namespace AirMet.Controllers {
 
@@ -29,32 +24,6 @@ namespace AirMet.Controllers {
         public IActionResult Index()
         {
             List<Property> properties = _propertyDbContext.Properties.ToList();
-            foreach (var property in properties)
-            {
-                if (property.ImageData != null)
-                {
-                    var image = SixLabors.ImageSharp.Image.Load(property.ImageData);
-
-                    // Resize the image to a smaller size (e.g., 200x150)
-                    image.Mutate(x => x.Resize(new ResizeOptions
-                    {
-                        Size = new Size(200, 150), // Adjust size as needed
-                        Mode = ResizeMode.Max
-                    }));
-
-                    // Optimize the image quality (adjust quality as needed)
-                    var jpegEncoder = new JpegEncoder
-                    {
-                        Quality = 70 // Adjust quality as needed (70 is just an example)
-                    };
-
-                    var memoryStream = new MemoryStream();
-                    image.Save(memoryStream, jpegEncoder);
-
-                    property.ImageData = memoryStream.ToArray();
-                    property.ImageMimeType = "image/jpeg"; // Set the MIME type accordingly
-                }
-            }
             var itemListViewModel = new PropertyListViewModel(properties, "Index");
             return View(itemListViewModel);
         }
@@ -68,32 +37,6 @@ namespace AirMet.Controllers {
             var property = _propertyDbContext.Properties.FirstOrDefault(i => i.PropertyId == id);
             if (property == null)
                 return NotFound();
-
-            // Resize and optimize the image if ImageData is not null
-            if (property.ImageData != null)
-            {
-                var image = SixLabors.ImageSharp.Image.Load(property.ImageData);
-
-                // Resize the image to a specific size (e.g., 400x300)
-                image.Mutate(x => x.Resize(new ResizeOptions
-                {
-                    Size = new Size(400, 300),
-                    Mode = ResizeMode.Max
-                }));
-
-                // Optimize the image quality (adjust quality as needed)
-                var jpegEncoder = new JpegEncoder
-                {
-                    Quality = 70 // Adjust quality as needed (70 is just an example)
-                };
-
-                var memoryStream = new MemoryStream();
-                image.Save(memoryStream, jpegEncoder);
-
-                property.ImageData = memoryStream.ToArray();
-                property.ImageMimeType = "image/jpeg"; // Set the MIME type accordingly
-            }
-
             return View(property);
         }
 
@@ -102,26 +45,20 @@ namespace AirMet.Controllers {
         {
             return View();
         }
+
         [HttpPost]
-        public IActionResult Create(Property property, IFormFile image)
+        public IActionResult Create(Property property)
         {
             if (ModelState.IsValid)
             {
-                if (image != null && image.Length > 0)
-                {
-                    using (var memoryStream = new MemoryStream())
-                    {
-                        image.CopyTo(memoryStream);
-                        property.ImageData = memoryStream.ToArray();
-                        property.ImageMimeType = image.ContentType;
-                    }
-                }
                 _propertyDbContext.Properties.Add(property);
                 _propertyDbContext.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
             return View(property);
         }
+
+
 
     }
 }
--- AirMet/Models/PropertyDbContext.cs
+++ AirMet/Models/PropertyDbContext.cs
@@ -4,19 +4,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AirMet.Models
 {
-	public class PropertyDbContext : DbContext
-	{
-		public PropertyDbContext(DbContextOptions<PropertyDbContext> options) : base(options)
+
+    public class PropertyDbContext : DbContext
+    {
+        public PropertyDbContext(DbContextOptions<PropertyDbContext> options) : base(options)
         {
             Database.EnsureCreated();
         }
-        public DbSet<Property> Properties { get; set; }
-        //public DbSet<Customer> Customers { get; set; }
-        //public DbSet<Guest> Guests { get; set; }
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-         //   optionsBuilder.UseLazyLoadingProxies();
-        //}
+        public DbSet<Property> Properties { get; set; }
     }
+
 }
