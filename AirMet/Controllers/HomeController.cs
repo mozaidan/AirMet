8b0f54d4d721d98db79edf17e95edb63e38111ab
mohanadal-falah <s362092@oslomet.no>
2023-10-04 17:32:33 +0200

    update and delete

--- AirMet/Controllers/HomeController.cs
+++ AirMet/Controllers/HomeController.cs
@@ -83,7 +83,97 @@ namespace AirMet.Controllers {
             return View(property);
         }
 
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var property = _propertyDbContext.Properties.Include(p => p.Images).FirstOrDefault(i => i.PropertyId == id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+            return View(property);
+        }
 
+        [HttpPost]
+        public IActionResult Update(int id, Property updatedProperty)
+        {
+            if (ModelState.IsValid)
+            {
+                updatedProperty.PropertyId = id;
+
+                // Fetch the existing property from the database
+                var propertyFromDb = _propertyDbContext.Properties.Include(p => p.Images)
+                                      .FirstOrDefault(i => i.PropertyId == updatedProperty.PropertyId);
+
+                if (propertyFromDb == null)
+                {
+                    return NotFound();
+                }
+
+                // Update the properties of the existing property
+                propertyFromDb.Description = updatedProperty.Description;
+                propertyFromDb.Price = updatedProperty.Price;
+                propertyFromDb.Address = updatedProperty.Address;
+
+                // If there are new files/images uploaded
+                if (updatedProperty.Files != null && updatedProperty.Files.Count > 0)
+                {
+                    var uploads = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images");
+
+                    if (!Directory.Exists(uploads))
+                    {
+                        Directory.CreateDirectory(uploads);
+                    }
+
+                    // Save the new images
+                    foreach (var file in updatedProperty.Files)
+                    {
+                        var fileName = Path.GetFileName(file.FileName);
+                        var filePath = Path.Combine(uploads, fileName);
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            file.CopyTo(fileStream);
+                        }
+
+                        var newImage = new PropertyImage { ImageUrl = $"/images/{fileName}", PropertyId = propertyFromDb.PropertyId };
+                        _propertyDbContext.PropertyImages.Add(newImage);
+                        propertyFromDb.Images.Add(newImage);
+                    }
+                }
+
+                _propertyDbContext.Properties.Update(propertyFromDb);
+                _propertyDbContext.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(updatedProperty);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var property = _propertyDbContext.Properties.Find(id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+            return View(property);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var property = _propertyDbContext.Properties.Find(id);
+            if (property == null)
+            {
+                return NotFound();
+            }
+            _propertyDbContext.Properties.Remove(property);
+            _propertyDbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        
 
 
     }
