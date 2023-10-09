ab1df815602f0d229a12efaab45721ebb8762739
Abdulaaziz9999 <97537036+Abdulaaziz9999@users.noreply.github.com>
2023-10-09 15:09:10 +0200

    her i'v add in modeler.property and person

--- AirMet/Models/Person.cs
+++ AirMet/Models/Person.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace AirMet.Models;
 public class Person
 {
+
+    [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ. \-]{2,20}", ErrorMessage = "The Name must contain between 1 and 20 characters")]
+
     public string Name { get; set; } = string.Empty;
     public string Address { get; set; } = string.Empty;
-    public int Old { get; set; }
+    public int Age { get; set; }
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")]
+
     public string PhoneNr { get; set; } = string.Empty;
 }
--- AirMet/Models/Property.cs
+++ AirMet/Models/Property.cs
@@ -6,11 +6,25 @@ namespace AirMet.Models
 {
 	public class Property
 	{
+
+        //her i wanna to add soverome , type ,senger og guster and antall pepo
 		public int PropertyId { get; set; }
 		public decimal Price { get; set; }
-        public int sovereom { get; set; }   
         public string Address { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        [StringLength(400)]
+        public string Description{ get; set; } = string.Empty;
+
+        [Range(1, 4, ErrorMessage = "number of bed must be 0-4")]
+
+        public int NumberOfBed { get; set; }
+
+
+        [Range(1, 20, ErrorMessage = "number of rooms must be 0-10")]
+        public int  NumberOfRom { get; set; }   
+
+        public bool isAvailable { get; set; }   
+
+        public 
         public virtual List<PropertyImage> Images { get; set; } = new List<PropertyImage>();
         [NotMapped]
         public virtual List<IFormFile>? Files { get; set; }
